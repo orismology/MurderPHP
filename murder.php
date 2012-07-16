72ca6ac0977f253d56ea66f152eff0bb5e09ae26
Dominic Mulligan <dom@orismology.me>
2012-07-16 22:00:13 +1000

    Added the beginnings of murder.php, but it's not a framework yet!
    Also fixed more spellings in readme

--- murder.php
+++ murder.php
@@ -1,5 +1,9 @@
-MurderPHP
-=========
+<?php 
+
+/* 
+
+MurderPHP  - Copyright (C) 2012 Adam D. and Dominic M. 
+=============================================================
 
 MurderPHP is a PHP framework for bloodying up your PHP scripts. 
 
@@ -7,3 +11,8 @@ Well, when we say framework, we really just mean function set.
 
 And when we say function set, we really mean that we've just renamed
 a whole bunch of existing PHP functions so that they're more gruesome.
+
+
+*/
+
+?>
