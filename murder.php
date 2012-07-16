8a85c1af62388553aa9d42b19fe1b8a8e0734d79
Dominic Mulligan <dom@orismology.me>
2012-07-16 22:21:20 +1000

    Add: murder(), assassinate()

--- murder.php
+++ murder.php
@@ -15,4 +15,15 @@ a whole bunch of existing PHP functions so that they're more gruesome.
 
 */
 
+// Here it is, the original and the best
+function murder($murder = null){
+  die('<marquee style="font-family: \'Comic Sans MS\'; font-size: 100px;"><blink>'.$murder.'</blink></marquee>');
+  // Adapted from adamd's implementation on 16 Jul 12, 12:36 AM
+}
+
+// A quieter version, accepts no parameters
+function assassinate(){
+  murder();
+}
+
 ?>
