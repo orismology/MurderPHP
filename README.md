be30e1ac14c0efe507ee86507fd46763a86b38ff
orismology <dom@orismology.me>
2014-11-12 00:53:15 +1100

    Update 12/11/14

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ Well, when we say framework, we really just mean function set.
 
 And when we say function set, we really mean that we've just renamed
 a whole bunch of existing PHP functions so that they're more gruesome.
+
+## Current Function Set
+
+murder($message) - like die()
+assassinate - like murder(), but quieter - accepts no paramaters
+print\_ahh() - like print\_r()
+amputate() - like substr()
+
+## Changelog
+
+12/11/14 - Added print_ahh() and amputate(), fixed murder() to avoid blank <marquee>
--- murder.php
+++ murder.php
@@ -2,7 +2,7 @@
 
 /* 
 
-MurderPHP  - Copyright (C) 2012 Adam D. and Dominic M. 
+MurderPHP  - Copyright (C) 2012-14 Adam D. and Dominic M. 
 =============================================================
 
 MurderPHP is a PHP framework for bloodying up your PHP scripts. 
@@ -17,8 +17,12 @@ a whole bunch of existing PHP functions so that they're more gruesome.
 
 // Here it is, the original and the best
 function murder($murder = null){
-  die('<marquee style="font-family: \'Comic Sans MS\'; font-size: 100px;"><blink>'.$murder.'</blink></marquee>');
-  // Adapted from adamd's implementation on 16 Jul 12, 12:36 AM
+	if ($murder == null){
+		die(); // Added, becuase we don't want blank marquee tags, do we?
+	}else{
+ 		 die('<marquee style="font-family: \'Comic Sans MS\'; font-size: 100px;"><blink>'.$murder.'</blink></marquee>');
+ 		 // Adapted from adamd's implementation on 16 Jul 12, 12:36 AM
+ 		 // Should probably think about changing this from marquee and blink to css3 animations
 }
 
 // A quieter version, accepts no parameters
@@ -26,4 +30,16 @@ function assassinate(){
   murder();
 }
 
+// Because we can't have ! in function names, ahh by itself will have to do
+function print_ahh($expression, $return = false ){
+	print_r($expression, $return);
+}
+
+function amputate($string,$start,$length){
+	if(isset($length)){
+		substr($string,$start,$length);
+	}else{
+		substr($string,$start);
+	}
+}
 ?>
